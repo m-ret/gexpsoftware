f4777ca1d385f98f93832e8293f8da3339d4da88
Andres Camacho Aguilar <andcamachoa@gmail.com>
2023-03-31 15:22:48 -0600

    feat(comps): logo-files-added

--- app/head.jsx
+++ app/head.jsx
@@ -4,7 +4,7 @@ export default function Head() {
       <title>GEXP Software - Embrace & Drive Change - Software Agency</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="" />
-      <link rel="icon" href="/images/favicon.ico" />
+      <link rel="icon" href="../../../favicon.ico" />
     </>
   );
 }
