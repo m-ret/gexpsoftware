ff712771fc7fb11ea84c6da4b0e177277990f125
Andres Camacho Aguilar <andcamachoa@gmail.com>
2023-03-31 13:20:19 -0600

    feat(comps): font-format-style

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,17 +5,21 @@ import Header from "@/components/Header";
 import ScrollToTop from "@/components/ScrollToTop";
 import "node_modules/react-modal-video/css/modal-video.css";
 import "../styles/index.css";
+import localFont from '@next/font/local'
+
+const myFont = localFont({ src:'../public/fonts/PPNeueMontreal-Medium.otf' })
+
 
 export default function RootLayout({ children }) {
   return (
-    <html suppressHydrationWarning lang="en">
+    <html suppressHydrationWarning lang="en" className="font-NeueMontreal" >
       {/*
         <head /> will contain the components returned by the nearest parent
         head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
 
-      <body className="dark:bg-black">
+      <body className={myFont.className}>
         <Providers>
           <Header />
           {children}
--- app/page.jsx
+++ app/page.jsx
@@ -7,9 +7,7 @@ import Features from "@/components/Features";
 import Hero from "@/components/Hero";
 import Testimonials from "@/components/Testimonials";
 import Video from "@/components/Video";
-import { Inter } from "@next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
