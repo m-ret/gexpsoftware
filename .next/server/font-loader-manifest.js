b6612454efd3fdfce8badde5f4230e78d77235af
m-ret <maketroli@gmail.com>
2023-04-15 10:43:30 -0600

    Add the repo again

--- .next/server/font-loader-manifest.js
+++
@@ -1,8 +0,0 @@
-self.__FONT_LOADER_MANIFEST={
-  "pages": {},
-  "app": {
-    "/Users/mrs/Work/gexpsoftware/node_modules/@next/font/local/target.css?{\"path\":\"app/layout.jsx\",\"import\":\"\",\"arguments\":[{\"src\":\"../public/fonts/PPNeueMontreal-Medium.otf\"}],\"variableName\":\"myFont\"}": [
-      "static/media/51731d05c2347afe.p.otf"
-    ]
-  }
-}
