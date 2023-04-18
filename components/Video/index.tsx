aeaa4e869360a679cfeccf1a2180d2a6d9057a1d
Alonso Salguero <jalonsc66@gmail.com>
2023-04-17 21:20:04 -0600

    Se quita modal video, se desactiva opciones del sub menu, se comentan sign in y sign up

--- components/Video/index.tsx
+++ components/Video/index.tsx
@@ -21,12 +21,12 @@ const Video = () => {
 
         <div className="-mx-4 flex flex-wrap">
           <div className="w-full px-4">
-            <div
+           {/* <div
               className="wow fadeInUp mx-auto max-w-[770px] overflow-hidden rounded-md"
               data-wow-delay=".15s"
             >
               <div className="relative aspect-[77/40] items-center justify-center">
-                <Image src="/images/video/video.jpg" alt="video image" fill />
+                <Image src="/images/video/video.jpg" alt="video image" fill /> 
                 <div className="absolute top-0 right-0 flex h-full w-full items-center justify-center">
                   <button
                     onClick={() => setOpen(true)}
@@ -41,14 +41,15 @@ const Video = () => {
                       <path d="M15.5 8.13397C16.1667 8.51888 16.1667 9.48112 15.5 9.86602L2 17.6603C1.33333 18.0452 0.499999 17.564 0.499999 16.7942L0.5 1.20577C0.5 0.43597 1.33333 -0.0451549 2 0.339745L15.5 8.13397Z" />
                     </svg>
                   </button>
-                </div>
+                </div> 
               </div>
-            </div>
+            </div> */}
+            
           </div>
         </div>
       </div>
 
-      <ModalVideo
+    {/*  <ModalVideo
         channel="youtube"
         autoplay={false}
         start={false}
@@ -56,7 +57,7 @@ const Video = () => {
         videoId="L61p2uyiMSo"
         onClose={() => setOpen(false)}
         
-      />
+  /> */}
 
       <div className="absolute bottom-0 left-0 right-0 z-[-1]">
         <img src="/images/video/shape.svg" alt="shape" className="w-full" />
