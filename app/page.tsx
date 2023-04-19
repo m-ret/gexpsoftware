34bff0d3ad03467bc1f967046e55c7012a5df861
Alonso Salguero <jalonsc66@gmail.com>
2023-04-18 23:46:52 -0600

    se cambian iconos, se modifican textos, se habilita modal de subcribe, de comentan testimonials, precios y blog del home

--- app/page.tsx
+++ app/page.tsx
@@ -23,9 +23,9 @@ export default function Home() {
       <Brands />
       <AboutSectionOne />
       <AboutSectionTwo />
-      <Testimonials />
-      <Pricing />
-      <Blog />
+     {/*<Testimonials /> */}
+     {/* <Pricing /> */}
+     {/*<Blog /> */} 
       <Contact />
     </>
   );
--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -14,14 +14,14 @@ const Footer = () => {
               <div className="mb-12 max-w-[360px] lg:mb-16">
                 <Link href="/" className="mb-8 inline-block">
                   <Image
-                    src="images/logo/logo-9.svg"
+                    src="images/logo/logo-12.svg"
                     alt="logo"
                     className="w-full dark:hidden"
                     width={140}
                     height={30}
                   />
                   <Image
-                    src="images/logo/logo-10.svg"
+                    src="images/logo/logo-11.svg"
                     alt="logo"
                     className="hidden w-full dark:block"
                     width={140}
