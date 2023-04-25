b64adfe4631b74ee1be2bd5cded63893b3ffbff9
Alonso Salguero <jalonsc66@gmail.com>
2023-04-25 14:53:01 -0600

    Se aplica formato y eliminan espacios

--- components/TechStack/SingleTechStack.tsx
+++ components/TechStack/SingleTechStack.tsx
@@ -1,25 +1,19 @@
 import Image from "next/image";
 import { TechStackType } from "@/types/techstack";
 
-
 const SingleTechStack = ({ techstack }: { techstack: TechStackType }) => {
-    const { image, name } = techstack;
-  
-    return (
-  
-      <div className="mx-3 flex w-full max-w-[160px] items-center justify-center py-[15px] sm:mx-4 lg:max-w-[130px] xl:mx-6 xl:max-w-[150px] 2xl:mx-8 2xl:max-w-[160px]">
-        <a      
-          target="_blank"
-          rel="nofollow noreferrer"
-          className="relative h-10 w-full transition opacity-100 dark:hover:opacity-100"
-        >
-          <Image src={image} alt={name} fill />
-        </a>
-  
-  
-      </div>
-    
-    );
-  };
+  const { image, name } = techstack;
 
+  return (
+    <div className="mx-3 flex w-full max-w-[160px] items-center justify-center py-[15px] sm:mx-4 lg:max-w-[130px] xl:mx-6 xl:max-w-[150px] 2xl:mx-8 2xl:max-w-[160px]">
+      <a
+        target="_blank"
+        rel="nofollow noreferrer"
+        className="relative h-10 w-full opacity-100 transition dark:hover:opacity-100"
+      >
+        <Image src={image} alt={name} fill />
+      </a>
+    </div>
+  );
+};
 export default SingleTechStack;
