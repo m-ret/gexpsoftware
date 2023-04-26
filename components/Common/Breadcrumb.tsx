cac67e06a188220dbfb2ba6f7db31dc702efef91
m-ret <maketroli@gmail.com>
2023-04-26 00:07:00 -0600

    Adjust colors on texts

--- components/Common/Breadcrumb.tsx
+++ components/Common/Breadcrumb.tsx
@@ -1,8 +1,8 @@
-import Link from "next/link";
+import Link from 'next/link';
 
 const Breadcrumb = ({
   pageName,
-  description,
+  description
 }: {
   pageName: string;
   description: string;
@@ -14,10 +14,10 @@ const Breadcrumb = ({
           <div className="-mx-4 flex flex-wrap items-center">
             <div className="w-full px-4 md:w-8/12 lg:w-7/12">
               <div className="mb-8 max-w-[570px] md:mb-0 lg:mb-12">
-                <h1 className="mb-5 text-2xl font-bold text-black dark:text-white sm:text-3xl">
+                <h1 className="mb-5 text-2xl font-bold text-black dark:text-white sm:text-5xl">
                   {pageName}
                 </h1>
-                <p className="text-base font-medium leading-relaxed text-body-color">
+                <p className="text-base font-medium leading-relaxed text-body-color dark:text-white">
                   {description}
                 </p>
               </div>
